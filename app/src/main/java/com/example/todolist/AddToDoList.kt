ff579f914e839f09a00970c461267a2c3d4e4ed9
arnischairiah <arnischairiah94184@gmail.com>
2023-12-11 12:29:57 +0700

    commit

---
+++ app/src/main/java/com/example/todolist/AddToDoList.kt
@@ -0,0 +1,11 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddToDoList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_to_do_list)
+    }
+}
---
+++ app/src/main/java/com/example/todolist/ToDoList.kt
@@ -0,0 +1,3 @@
+package com.example.todolist
+
+data class ToDoList(val id:Int, val title:String, val content: String)
